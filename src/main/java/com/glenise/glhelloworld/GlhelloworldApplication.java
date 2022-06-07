373d811a2d7b17881a11af4873143f457bf90a8e
saravanan <tosaravan@yahoo.co.uk>
2022-06-07 10:46:07 +0100

    initial version

---
+++ src/main/java/com/glenise/glhelloworld/GlhelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.glenise.glhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlhelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlhelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glenise/glhelloworld/GlhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glenise.glhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlhelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
